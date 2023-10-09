a42ed144298ecde8b86761c4e4c59707a16fd190
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-09 08:42:53 -0400

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,11 @@
+function celsiusToFarenheit(event){
+    event.preventDefault()
+    const farenheit = event.target.farenheit.value
+    const celsius = 5 / 9 * (farenheit-32)
+    const h2 = document.querySelector('h2')
+    h2.innerText = farenheit + "\u00B0F is " + celsius +"\u00B0C"
+}
+
+const form = document.querySelector('form')
+form.addEventListener('submit',celsiusToFarenheit)
+
